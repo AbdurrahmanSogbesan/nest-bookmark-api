5a9ebdf4d0a69b8917170eed1b641c2f7b409cf4
Abdurrahman <abdurrahman0803@gmail.com>
2023-09-24 05:01:25 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document, {
     // Manual swagger options for vercel deployment
-    customSiteTitle: 'Backend Generator',
+    customSiteTitle: 'Bookmark API',
     customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
     customJs: [
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
