8dd84870392ae6d0f5a8039836d5703f4a48f57e
Abdurrahman <abdurrahman0803@gmail.com>
2023-09-24 03:53:38 +0100

    update vercel config

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,8 @@ async function bootstrap() {
     .addBearerAuth()
     .setTitle('Bookmark API')
     .setDescription('Manage bookmarks')
-    .setVersion("1.0").build()
+    .setVersion('1.0')
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
